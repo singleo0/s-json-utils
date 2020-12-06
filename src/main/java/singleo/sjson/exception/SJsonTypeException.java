211785eec9d1fda89938608ff1614c1f496faadf
singleo <2824964425@qq.com>
2020-12-06 13:09:34 +0800

    修改描述

--- src/main/java/singleo/sjson/exception/SJsonTypeException.java
+++ src/main/java/singleo/sjson/exception/SJsonTypeException.java
@@ -4,7 +4,7 @@ import singleo.sjson.entity.JsonType;
 
 public class SJsonTypeException extends SJsonException {
     public SJsonTypeException(JsonType jsonType, String keyPath, String key, Object jsonObject){
-        super("期望jsonObject为JSONObject或JSONArray,但为: "+jsonType.toString()+" 当前为keyPath: "+keyPath+" 当前key: "+key+", jsonObject: "+jsonObject.toString() );
+        super("期望jsonObject为JSONObject或JSONArray,但为: "+jsonType.toString()+" 当前keyPath: "+keyPath+" 当前key: "+key+", jsonObject: "+jsonObject.toString() );
     }
     public SJsonTypeException(String message){
         super(message);
